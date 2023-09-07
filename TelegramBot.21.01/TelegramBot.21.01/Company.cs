9db6427c23769faed93d5c92cd859da9e2748cea
Galyautdinov Ilya <43208661+galyautdinov@users.noreply.github.com>
2023-09-07 16:17:05 +0300

    Add files via upload

---
+++ TelegramBot.21.01/TelegramBot.21.01/Company.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotApp
+{
+    public class Company
+    {
+        public string FullName { get; set; }
+        public string ShortName { get; set; }
+        public string Ogrn { get; set; }
+        public string Inn { get; set; }
+        public string Kpp { get; set; }
+
+        public string __typename { get; set; }
+        public AuthorizedCapital AuthorizedCapital { get; set; }
+        public List<Okveds> Okveds { get; set; }
+        public HeadMain Head { get; set; }
+        public Status Status { get; set; }
+        public string TerminationDate { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Ogrnip { get; set; }
+
+        public Address Address { get; set; }
+        public Registration Registration { get; set; }
+        public Extracts Extracts { get; set; }
+        public List<FounderMain> Founders { get; set; }
+        public Taxation Taxation { get; set; }
+        public Compliance Compliance { get; set; }
+        public Finances Finances { get; set; }
+    }
+}
---
+++ TelegramBot.21.01/TelegramBot.21.01/FinancialStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotApp
+{
+    public class FinancialStatus
+    {
+        public string Year { get; set; }
+        public long Value { get; set; }
+        public string ComparisonPercent { get; set; }
+    }
+}
